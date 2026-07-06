223728fbc213e6448eb94355576f8e3c9d951115
kandy164 <emmanuelkandy66@gmail.com>
2026-07-07 00:57:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Handle Yes button click
 document.getElementById("yesBtn").addEventListener("click", () => {
-  alert("Kandy Jr says I love you too 💖 Let's go on a date!");
+  alert("Kandy Jr says I love you too 💖 Let's go on a date! Message me here 👉 https://www.facebook.com/Cowboykandy");
 });
+
 
 // Handle No button hover/tap (make it run away)
 const noBtn = document.getElementById("noBtn");
